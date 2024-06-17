28a4ad302bc629d9a42a53283c93884fb649fd72
Brijesh Yadav <Vasant@22>
2024-06-17 14:16:16 +0200

    Updated backend code to enable Sling model exporter support

--- core/src/main/java/com/adobe/aem/guides/rnd/core/models/SegmentDataSourceModel.java
+++ core/src/main/java/com/adobe/aem/guides/rnd/core/models/SegmentDataSourceModel.java
@@ -1,13 +1,9 @@
 package com.adobe.aem.guides.rnd.core.models;
 
-
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-
 public interface SegmentDataSourceModel {
-    @ValueMapValue
     String getName();
 
-    @ValueMapValue
     String getSegmentTags();
+
     boolean isEmpty();
 }
--- core/src/main/java/com/adobe/aem/guides/rnd/core/models/impl/BylineModelimpl.java
+++ core/src/main/java/com/adobe/aem/guides/rnd/core/models/impl/BylineModelimpl.java
@@ -1,10 +1,12 @@
 package com.adobe.aem.guides.rnd.core.models.impl;
 
 import com.adobe.aem.guides.rnd.core.models.BylineModel;
+import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.wcm.core.components.models.Image;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.Self;
@@ -22,7 +24,7 @@ import java.util.List;
         resourceType = {BylineModelimpl.RESOURCE_TYPE},
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
-
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class BylineModelimpl implements BylineModel {
     protected static final String RESOURCE_TYPE = "rnd/components/byline";
 
